bdbc310a330bf0722f1710644d3eb58f107ddd36
Bruno069-star <bruno.koziel.barbosa@escola.pr.gov.br>
2026-05-22 10:34:50 -0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+let nome = ["Bruno","Matheus"]
+
+function busca (){
+    let nomeBusca = document.querySelector("#input").value;
+    for(let i = 0; i < nome.length; i++){
+        if(nomeBusca == nome[i]){
+            document.querySelector("#saída").textContent = 
+            nomeBusca + " encontrado";
+        }
+        break
+    }
+}
